a5eb6ced52620c4ceeb7abb35ebfb38b81bde4f4
Daniel Salazar Erazo <dsalazar1982@yahoo.com>
2021-05-26 13:45:13 -0500

    Update

--- Recursos/conexion.php
+++ Recursos/conexion.php
@@ -4,13 +4,9 @@ include("configuracion.php");
 
 $conexion = new mysqli($servidor, $usuario, $clave, $bd);
 
-if(mysqli_connect_errno()){
+if (mysqli_connect_errno()) {
     echo "No conectado ", mysqli_connect_error();
     exit();
-}else{
+} else {
     echo "Conectado";
 }
-
-
-
-?>
--- configuracion.php
+++ configuracion.php
@@ -4,7 +4,3 @@ $servidor = '10.0.0.51';
 $usuario = 'dba_user1';
 $clave = 'dba_user@MariaDB2020*';
 $bd = 'db_RegistroPHP';
-
-
-
-?>
