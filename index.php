42df478dbee20ee3ce0c125ebb49bb042835900e
Daniel Salazar Erazo <dsalazar1982@yahoo.com>
2021-05-31 10:53:30 -0500

    Update

--- index.php
+++ index.php
@@ -361,14 +361,14 @@ if (isset($_POST["registrar"])) {
 
 	<!-- inline scripts related to this page -->
 	<script type="text/javascript">
-		jQuery(function($) {
+		/*jQuery(function($) {
 			$(document).on('click', '.toolbar a[data-target]', function(e) {
 				e.preventDefault();
 				var target = $(this).data('target');
 				$('.widget-box.visible').removeClass('visible'); //hide others
 				$(target).addClass('visible'); //show target
 			});
-		});
+		});*/
 
 
 
