66ab46690192f95ab02313f5baea2ce36ef43e5e
DANIEL ALBERTO SALAZAR ERAZO <dasalazar@bellezae.local>
2022-06-02 21:07:52 -0500

    Actualizacion fichero configuracion.php

--- conexion.php
+++ conexion.php
@@ -2,8 +2,10 @@
 
 include("configuracion.php");
 
+// Se crea la conexión
 $conexion = new mysqli($servidor, $usuario, $clave, $bd);
 
+// Verifica la conexión
 if ($conexion->connect_error) {
     die("Fallo conexion: " . $conexion->connect_error);
 }
